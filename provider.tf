78955d7d5bc9a265beeaf78bf974208fc1927eb5
Abhishek sharma <52596897+abhishek7389@users.noreply.github.com>
2021-05-15 00:07:28 +0530

    provider.tf file updated

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = file("upheld-apricot-313105-d87362db5f3e.json")
- project     = "upheld-apricot-313105"
- region      = "us-central1"
+ credentials = "${file("<YOUR SERVICE ACCOUNT JSON KEY NAME>")}"
+ project     = "<YOUR PROJECT ID>"
+ region      = "<YOUR DEPLOYMENT REGION>"
 }
